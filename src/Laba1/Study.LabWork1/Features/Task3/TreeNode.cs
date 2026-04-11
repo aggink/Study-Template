52ff85499a4be8c8fba43d163ba9b7efb42789df
Ник <N1ck6@users.noreply.github.com>
2026-04-12 00:00:01 +0300

    Выполнено задание 3

---
+++ src/Laba1/Study.LabWork1/Features/Task3/TreeNode.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Study.LabWork1.Features.Task3
+{
+    public class TreeNode
+    {
+        public object Value { get; set; }
+
+        public List<TreeNode> Children { get; set; }
+
+        public TreeNode(object value)
+        {
+            Value = value;
+            Children = new List<TreeNode>();
+        }
+
+        public void AddChild(TreeNode child)
+        {
+            Children.Add(child);
+        }
+
+        public void PrintAllValues()
+        {
+            Console.WriteLine(Value);
+
+            if (Children.Count == 0) return;
+
+            foreach (var child in Children)
+            {
+                child.PrintAllValues();
+            }
+        }
+    }
+}
--- src/Laba1/Study.LabWork1/Shared/Services/RunService.cs
+++ src/Laba1/Study.LabWork1/Shared/Services/RunService.cs
@@ -1,3 +1,4 @@
+using Study.LabWork1.Features.Task3;
 using Study.LabWork1.Shared.Abstractions;
 
 namespace Study.LabWork1.Shared.Services;
@@ -20,5 +21,20 @@ public class RunService : IRunService
     /// <summary>
     /// Задание 3
     /// </summary>
-    public void RunTask3() => throw new NotImplementedException();
+    public void RunTask3()
+    {
+        Console.WriteLine("==== Тесты TreeNode ====");
+
+        var root = new TreeNode("Корень");
+        var child1 = new TreeNode("Потомок 1");
+        var child2 = new TreeNode("Потомок 2");
+        var grandChild1 = new TreeNode("Внук 1.1");
+
+        root.AddChild(child1);
+        root.AddChild(child2);
+        child1.AddChild(grandChild1);
+
+        Console.WriteLine("Результат вывода:");
+        root.PrintAllValues();
+    }
 }
