df99d1cb4e3f07ff69706fffdd87b2bceea120ca
Ник <N1ck6@users.noreply.github.com>
2026-04-12 00:12:02 +0300

    Minor changes

---
+++ src/Laba1/Study.LabWork1.UnitTests/Features/Task3/TreeNodeTests.cs
@@ -0,0 +1,27 @@
+﻿using Study.LabWork1.Features.Task3;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Study.LabWork1.UnitTests.Features.Task3
+{
+    public class TreeNodeTests
+    {
+        public static void Test()
+        {
+            Console.WriteLine("==== Тесты TreeNode ====");
+
+            var root = new TreeNode("Корень");
+            var child1 = new TreeNode("Потомок 1");
+            var child2 = new TreeNode("Потомок 2");
+            var grandChild1 = new TreeNode("Внук 1.1");
+
+            root.AddChild(child1);
+            root.AddChild(child2);
+            child1.AddChild(grandChild1);
+
+            Console.WriteLine("Результат вывода:");
+            root.PrintAllValues();
+        }
+    }
+}
