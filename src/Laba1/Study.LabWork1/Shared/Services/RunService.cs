e2965461c2534b6bcceff392c55f05e98e0f2acc
john code <A.S.Gooryev@yandex.ru>
2026-04-12 11:29:31 +0300

    lab1-task2

--- src/Laba1/Study.LabWork1/Shared/Services/RunService.cs
+++ src/Laba1/Study.LabWork1/Shared/Services/RunService.cs
@@ -84,7 +84,22 @@ public class RunService : IRunService
     /// <summary>
     /// Задание 2
     /// </summary>
-    public void RunTask2() => throw new NotImplementedException();
+    public void RunTask2()
+    {
+
+        var c = new Client();
+
+        var d1 = new Dud();
+        var dudAdapter = new Adapter(d1);
+        c.request(dudAdapter, 0);
+        c.request(dudAdapter, 1);
+        c.request(dudAdapter, 2);
+
+        var b1 = new RealBase();
+        c.request(b1, -1);
+        c.request(b1, -2);
+
+    }
 
     /// <summary>
     /// Задание 3
